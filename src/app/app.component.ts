37a68031273ae90b63380abac95df51bb0cbc620
Godwin Okoi <gordie2u@gmail.com>
2022-02-03 12:23:43 +0100

    finish router animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,109 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RouterOutlet } from '@angular/router';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnim', [
+      transition(':increment', [
+        style({
+          position: 'relative',
+          overflow: 'hidden'
+        }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          })
+        ], {optional: true}),
+
+        group([
+          query(':leave', [
+            animate('250ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(-80px)'
+            }))
+          ], {optional: true}),
+  
+          query(':enter', [
+            style({
+              transform: 'translateX(80px)',
+              opacity: '0'
+            }),
+            animate('250ms 100ms ease-out', style({
+              opacity: 1,
+              transform: 'translateX(0px)'
+            }))
+          ], {optional: true}),
+        ]),
+      ]),
+
+      transition(':decrement', [
+        style({
+          position: 'relative',
+          overflow: 'hidden'
+        }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          })
+        ], {optional: true}),
+
+        group([
+          query(':leave', [
+            animate('200ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(80px)'
+            }))
+          ], {optional: true}),
+  
+          query(':enter', [
+            style({
+              transform: 'translateX(-80px)',
+              opacity: '0'
+            }),
+            animate('200ms 100ms ease-out', style({
+              opacity: 1,
+              transform: 'translateX(0px)'
+            }))
+          ], {optional: true}),
+        ]),
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'personal-dashboard';
   imageUrl: any;
 
-  constructor(private http: HttpClient) {
-  
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getBackgroundUrl();
   }
   
   getBackgroundUrl() {
-    this.http.get("https://api.unsplash.com/photos/random/?client_id=iWWSRuz2J3efsFzG1e-xmS3W3f16y07teYLum-sN7kM").subscribe((data: any) => {
-      console.log('image', data.urls.full)
+    this.http.get("https://api.unsplash.com/photos/random/?client_id=iWWSRuz2J3efsFzG1e-xmS3W3f16y07teYLum-sN7kM")
+    .subscribe((data: any) => {
       this.imageUrl = data.urls.full;
     });
   }
+
+  prepareRoute(outlet: RouterOutlet) {
+    if(outlet.isActivated) {
+      return outlet.activatedRouteData['tab']
+    }
+  }
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import {HttpClientModule} from '@angular/common/http';
 
 
@@ -22,6 +23,7 @@ import { BookmarkTileComponent } from './bookmarks/bookmark-tile/bookmark-tile.c
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule
   ],
